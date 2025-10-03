4a68ee5f29fbc1d5db0c08afbeca66826db68558
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-10-03 09:42:45 +0200

    feat: Retrieve busy times work interval

---
+++ src/app/LandingPageModelService.ts
@@ -0,0 +1,3 @@
+export function createLandingPageModel() {
+    return 'Busy times';
+}
--- src/app/__tests__/LandingPageModelService.test.ts
+++ src/app/__tests__/LandingPageModelService.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+import { createLandingPageModel } from '../LandingPageModelService';
 
 describe('Landing page model service', () => {
     it('returns model with busy times work interval', () => {
